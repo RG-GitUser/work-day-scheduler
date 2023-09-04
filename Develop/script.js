e878d68a556fa458c74c54ceb9020adc625771bc
Riley <rileyg0035@gmail.com>
2023-09-03 22:44:03 -0300

    attempted to add save function to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -77,6 +77,14 @@ setClasses();
 
 
 
+localStorage.saveTask = " ";
+document.getElementById("hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14","hour-15", "hour-16", "hour-17").innerHTML = localStorage.saveTask;
+localStorage.removeItem("saveTask");
+
+document.getElementByClass('btn saveBtn col-2 col-md-1').addEventListener('save', function() {
+
+  localStorage.setItem('saveTask', saveTask);
+});
 
 
 
